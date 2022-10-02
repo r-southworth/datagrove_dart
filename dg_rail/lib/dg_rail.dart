3f18778c884bbab0409166cf1c52afa0a565e589
jim <jimh@datagrove.com>
2022-10-02 07:28:03 -0400

    a

--- dg_rail/lib/dg_rail.dart
+++ dg_rail/lib/dg_rail.dart
@@ -5,9 +5,18 @@ import 'package:flutter/cupertino.dart';
 // this is a widget to adaptively place tool buttons on left or bottom
 class DgRail extends StatelessWidget {
   Widget child;
-  List<Widget> tool;
+  List<Widget> leading=[], middle=[], trailing=[];
 
-  DgRail({super.key, required this.child, required this.tool});
+
+  DgRail({super.key, required this.child, 
+    List<Widget>? leading,
+        List<Widget>? middle,
+            List<Widget>? trailing,
+  }){
+    this.leading = leading ??[];
+    this.middle = middle ?? [];
+    this.trailing= trailing??[];
+  }
   @override
   Widget build(BuildContext context) {
     final wide = MediaQuery.of(context).size.width > 400;
@@ -23,7 +32,7 @@ class DgRail extends StatelessWidget {
                 SizedBox(
                   height: 0,
                 ),
-                for (final o in tool)
+                for (final o in leading)
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 8.0),
                     child: o,
@@ -45,5 +54,34 @@ class DgRail extends StatelessWidget {
         ]),
       );
     }
+  }
+}
+
+// user configurable rail with data model
+// rail has 3 sections: start, dynamic, end
+// dynamic will be tools that show for some context.
+
+class RailConfig {
+  List<Tool> start = [];
+  List<Tool> end = [];
+  List<Tool> dynamic = [];
+}
+class Tool {
+
+}
+
+typedef RailController = ValueNotifier<RailConfig>;
+
+class DgUserRail extends StatelessWidget {
+  Widget child;
+  RailController controller;
+  DgUserRail({required this.controller, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return DgRail(
+      tool: [],
+      child: child
+    )
   }
 }
